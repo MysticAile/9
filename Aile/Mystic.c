2ed12566929038cfc6d78892f6ce8f2361beeb36
MysticAile <1259387531@qq.com>
2019-10-18 19:11:03 +0800

    123

--- Aile/Mystic.c
+++ Aile/Mystic.c
@@ -1,10 +1,10 @@
 #include<stdio.h>
 
-float main(void)
+int main(void)
 {
     float r, v;
     r = 1.0;
-    v = (4/3) * (r^3) * 3;
-    printf("v is %if\n", v);
+    v = 4/3.0*r*r*r*3;
+    printf("v is %f\n", v);
     return 0;
 } 
