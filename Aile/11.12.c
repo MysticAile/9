fc74996bbba1b3e37754105c90f10e946c9fb8f2
MysticAile <1259387531@qq.com>
2019-11-12 10:10:25 +0800

    .

--- Aile/11.12.c
+++ Aile/11.12.c
@@ -1,50 +1,31 @@
 #include <stdio.h>
 int main(void)
 {
-    int a, mark;
+    int a;
     printf("Enter a:\n");
     scanf("%d", &a);
-        if (a >= 90)
-
-        {
-            mark=4;
-        }
-        else if (a >= 80)
-        {
-            mark=3;
-        }
-        else if (a >= 70)
-        {
-            mark=2;
-        }
-        else if (a >= 60)
-        {
-            mark=1;
-        }
-        else if (a < 60)
-        {
-            mark=0;
-        }
-    switch (mark)
+    for (a; a < 5; a++)
     {
-    case 4:
-        printf("Excellent");
-        break;
-    case 3:
-        printf("Good");
-        break;
-    case 2:
-        printf("Aveage");
-        break;
-    case 1:
-        printf("Poor");
-        break;
-    case 0:
-        printf("Failing");
-        break;
-    default:
-        printf("Error");
-        break;
+        switch (a)
+        {
+        case 4:
+            printf("grapes is 10.20\n");
+            break;
+        case 3:
+            printf("oranges is 4.10\n");
+            break;
+        case 2:
+            printf("pears is 2.50\n");
+            break;
+        case 1:
+            printf("apples is 3.00\n");
+            break;
+        case 0:
+            break;
+        default:
+            printf("0");
+            break;
+        }
     }
     return 0;
 }
