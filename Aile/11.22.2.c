35c4264a794846700d55c4c5bfde4f6d04668667
MysticAile <1259387531@qq.com>
2019-11-25 22:20:39 +0800

    .

--- Aile/11.22.2.c
+++ Aile/11.22.2.c
@@ -2,19 +2,11 @@
 int main(void)
 {
     int a, b, c;
-    for (a = 1; a <= 20; a++)
-        for (b = 1; b <= 33; b++)
-            for (c = 1; c <= 300; c++)
+    for (a = 0; a <= 20; a++)
+        for (b = 0; b <= 33; b++)
+            for (c = 0; c <= 300; c++)
             {
-                if (a * 5 + b * 3 + c / 3 == 100)
-                    
-
-                if (a + b + c == 100)
-                    
-
-                if (c % 3 == 0)
-                    
-
+                if (a * 5 + b * 3 + c / 3 == 100 && c % 3 == 0 && a + b + c == 100)
                 {
                     printf("公鸡: %d,母鸡: %d,雏鸡: %d\n", a, b, c);
                 }
