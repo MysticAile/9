491a51fc07034bc89d3cf7e874337f2e0ea92898
MysticAile <1259387531@qq.com>
2019-11-25 12:45:51 +0800

    。

--- Aile/11.22.2.c
+++ Aile/11.22.2.c
@@ -1,17 +1,23 @@
 #include <stdio.h>
-#include <math.h>
 int main(void)
 {
-    int i, a, b,c;
-    for (i = 100; i < 1000; i++)
-    {
-        a = pow(i / 100, 3);
-        b = pow((i - i / 100) / 10, 3);
-        c = pow(i % 10, 3);
-        if (i = a + b + c)
-        {
-            printf("%d", i);
-        }
-    }
+    int a, b, c;
+    for (a = 1; a <= 20; a++)
+        for (b = 1; b <= 33; b++)
+            for (c = 1; c <= 300; c++)
+            {
+                if (a * 5 + b * 3 + c / 3 == 100)
+                    
+
+                if (a + b + c == 100)
+                    
+
+                if (c % 3 == 0)
+                    
+
+                {
+                    printf("公鸡: %d,母鸡: %d,雏鸡: %d\n", a, b, c);
+                }
+            }
     return 0;
 }
